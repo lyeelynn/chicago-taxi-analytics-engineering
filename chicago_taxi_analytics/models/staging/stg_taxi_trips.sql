702159eaa45d1fbd1bf62109390578cbb86e205c
lyeelynn <lyeelynn.phang@email.com>
2026-07-19 16:59:52 +0800

    upload stg_taxi_trips

---
+++ chicago_taxi_analytics/models/staging/stg_taxi_trips.sql
@@ -0,0 +1,38 @@
+SELECT DISTINCT
+    TRIM(unique_key) AS unique_key,
+    TRIM(taxi_id) AS taxi_id,
+    trip_start_timestamp,
+    trip_end_timestamp,
+    trip_seconds,
+    trip_miles,
+    pickup_census_tract,
+    dropoff_census_tract,
+    pickup_community_area,
+    dropoff_community_area,
+    COALESCE(fare, 0) AS fare,
+    COALESCE(tips, 0) AS tips,
+    COALESCE(tolls, 0) AS tolls,
+    COALESCE(extras, 0) AS extras,
+    trip_total,
+    TRIM(payment_type) AS payment_type,
+    TRIM(company) AS company,
+    pickup_latitude,
+    pickup_longitude,
+    TRIM(pickup_location) AS pickup_location,
+    dropoff_latitude,
+    dropoff_longitude,
+    TRIM(dropoff_location) AS dropoff_location
+-- local testing
+FROM 'scripts/chicago_taxi.parquet' 
+-- for dbt prod
+-- FROM {{ source('chicago_taxi', 'taxi_trips') }}
+WHERE NULLIF(TRIM(unique_key),'') IS NOT NULL
+AND NULLIF(TRIM(taxi_id),'') IS NOT NULL
+AND trip_start_timestamp IS NOT NULL
+AND trip_seconds > 30
+AND (
+    COALESCE(fare, 0) + 
+    COALESCE(tips, 0) + 
+    COALESCE(tolls, 0) + 
+    COALESCE(extras, 0)
+    ) > 0
