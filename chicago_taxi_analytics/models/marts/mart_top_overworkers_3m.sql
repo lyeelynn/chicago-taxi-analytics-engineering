81b33dcbf4f86a36a3ad7f49d310f844f2fbe683
lyeelynn <lyeelynn.phang@email.com>
2026-07-21 00:12:41 +0800

    upload mart_top_overworkers_3m

---
+++ chicago_taxi_analytics/models/marts/mart_top_overworkers_3m.sql
@@ -0,0 +1,70 @@
+WITH cte_date AS (
+    SELECT 
+        DATE(MAX(session_start_timestamp) - INTERVAL 3 month) as period_start_date,
+        DATE(MAX(session_end_timestamp)) as period_end_date
+    -- for local test
+    FROM 'int_taxi_work_sessions.parquet'
+    -- for dbt prod
+    -- FROM {{ ref('int_taxi_work_sessions') }}  
+),
+
+cte_agg AS (
+    SELECT
+        taxi_id,
+
+        -- sessions
+        COUNT(DISTINCT session_id) as total_sessions,
+        SUM(session_duration_hours) as total_working_hours,
+        MAX(session_duration_hours) as max_working_hours,
+        SUM(total_trips) as total_trips,
+        SUM(session_duration_hours)/COUNT(DISTINCT session_id) working_hours_per_session,
+        SUM(total_trips)/COUNT(DISTINCT session_id) as trips_per_session,
+
+        -- break hours
+        SUM(break_before_session_hours)/COUNT(DISTINCT session_id) as avg_break_hours,
+        MIN(break_before_session_hours) as min_break_hours,
+
+        -- overworking indicators
+        SUM(is_long_shift) as long_shift_count,
+        SUM(is_long_shift)/COUNT(DISTINCT session_id) as long_shift_pct,
+        SUM(is_short_break) as short_break_count,
+        SUM(is_short_break)/COUNT(DISTINCT session_id) as short_break_pct
+
+    -- for local test 
+    FROM 'int_taxi_work_sessions.parquet'
+    -- for dbt prod
+    -- FROM {{ ref('int_taxi_work_sessions') }}  
+    WHERE DATE(session_start_timestamp) >= (
+        SELECT period_start_date
+        FROM cte_date
+    )
+    GROUP BY 1
+),
+
+cte_ranked AS (
+    SELECT 
+        *,
+        PERCENT_RANK() OVER (ORDER BY total_working_hours) AS total_working_hours_percentile
+    FROM cte_agg
+),
+
+cte_scored AS (
+    SELECT 
+        *,
+        (
+            (total_working_hours_percentile * 0.5) + -- 50% weightage: working hours
+            (long_shift_pct * 0.3) + -- 30% weightage: long shifts
+            (short_break_pct * 0.2) -- 20% weightage: short_breaks
+        ) as overworker_score
+    FROM cte_ranked
+    WHERE long_shift_pct >= 0 -- at least 30% shifts are long hours
+    AND short_break_count > 0 -- at least 1 short break
+)
+
+SELECT 
+    date.period_start_date,
+    date.period_end_date,
+    scored.*,
+    DENSE_RANK() OVER(ORDER BY overworker_score DESC) as overworker_rank
+FROM cte_scored scored
+CROSS JOIN cte_date date
