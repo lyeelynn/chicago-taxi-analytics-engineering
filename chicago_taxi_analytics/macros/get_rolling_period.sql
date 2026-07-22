06c18e5dfac7424ba27718cdd4a3e735d2a4a036
lyeelynn <lyeelynn.phang@email.com>
2026-07-22 20:39:10 +0800

    update date format in macro

--- chicago_taxi_analytics/macros/get_rolling_period.sql
+++ chicago_taxi_analytics/macros/get_rolling_period.sql
@@ -1,7 +1,7 @@
 {% macro get_rolling_period(date_column, source_relation, months_back) %}
 
     SELECT
-        DATE(MAX({{ date_column }}) - INTERVAL {{ months_back }} MONTH)
+        DATE(MAX({{ date_column }})) - INTERVAL {{ months_back }} MONTH
             AS period_start_date,
 
         DATE(MAX({{ date_column }}))
