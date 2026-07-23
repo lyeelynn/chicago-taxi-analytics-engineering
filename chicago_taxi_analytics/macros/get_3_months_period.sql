88e288691f28ac47a3d40d7dd8fb64228b48f6a4
lyeelynn <lyeelynn.phang@email.com>
2026-07-23 22:12:28 +0800

    remove unused macro

--- chicago_taxi_analytics/macros/get_3_months_period.sql
+++
@@ -1,8 +0,0 @@
-{% macro get_3_month_period(date_column, source_relation) %}
-
-    {{ date_column }} >= (
-        SELECT DATE(MAX({{ date_column }}) - INTERVAL 3 MONTH)
-        FROM {{ source_relation }}
-    )
-
-{% endmacro %}
